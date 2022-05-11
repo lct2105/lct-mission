051aa4b91ae454a0442ed1722fa80a1ac108e80e
lct2105 <105337020+lct2105@users.noreply.github.com>
2022-05-12 01:36:52 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'LCT-MISSION | Nhiệm vụ hằng ngày | © LCT (NYO) or DT SHOP'
+version '1.0.0'
+
+
+
+client_script {
+    'config.lua',
+    'client/main.lua'
+}
+
+
+server_script {
+    'config.lua',
+    'server/main.lua'
+}
+
+lua54 'yes'
